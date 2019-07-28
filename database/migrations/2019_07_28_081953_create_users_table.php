c302511193e293f7d638c749dd337602d9dc0d9b
wahyuadepratama <1511521024@student.unand.ac.id>
2019-07-28 16:04:31 +0700

    add migration

--- database/migrations/2019_07_28_081953_create_users_table.php
+++ database/migrations/2019_07_28_081953_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,14 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('users', function (Blueprint $table) {
+            $table->integer('nik');
+            $table->string('name');
+            $table->string('position');
+            $table->string('section');
+            $table->timestamps();
+
+            $table->primary('nik');
         });
     }
 
@@ -27,6 +31,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('users');
     }
 }
