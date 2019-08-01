c6db12fb377912338b2b2ee2154ae9eb2afdb842
wahyuadepratama <1511521024@student.unand.ac.id>
2019-08-01 22:56:45 +0700

    modul report fix

--- resources/views/layout/menu_user.blade.php
+++ resources/views/layout/menu_user.blade.php
@@ -1,27 +1,27 @@
 <!-- menu sidebar -->
 @section('sidebar')
-<a href="{{ url('/user/home') }}" class="{{ request()->is('user/home') ? 'active-btn' : '' }} list-group-item list-group-item-action bg-light"><i class="fa fa-home"></i> Home</a>
-<a href="{{ url('/user/lapor') }}" class="{{ request()->is('user/lapor') ? 'active-btn' : '' }} list-group-item list-group-item-action bg-light"><i class="fa fa-volume-up"></i> Lapor Bahaya</a>
-<a href="{{ url('/user/riwayat') }}" class="{{ request()->is('user/riwayat') ? 'active-btn' : '' }} list-group-item list-group-item-action bg-light"><i class="fa fa-history"></i> Riwayat pelaporan</a>
-<a href="{{ url('/user/statistik') }}" class="{{ request()->is('user/statistik') ? 'active-btn' : '' }} list-group-item list-group-item-action bg-light"><i class="fa fa-bar-chart"></i> Statistik</a>
-<a href="{{ url('/user/summary') }}" class="{{ request()->is('user/summary') ? 'active-btn' : '' }} list-group-item list-group-item-action bg-light"><i class="fa fa-print"></i> Summary</a>
+<a href="{{ url('/user/home') }}" class="{{ request()->is('user/home') ? 'active-btn' : '' }} list-group-item list-group-item-action bg-light"><i class="fa fa-home"></i> &nbsp;Home</a>
+<a href="{{ url('/user/lapor') }}" class="{{ request()->is('user/lapor') ? 'active-btn' : '' }} list-group-item list-group-item-action bg-light"><i class="fa fa-volume-up"></i> &nbsp;Lapor Bahaya</a>
+<a href="{{ url('/user/riwayat') }}" class="{{ request()->is('user/riwayat') ? 'active-btn' : '' }} list-group-item list-group-item-action bg-light"><i class="fa fa-history"></i> &nbsp;Riwayat pelaporan</a>
+<a href="{{ url('/user/statistik') }}" class="{{ request()->is('user/statistik') ? 'active-btn' : '' }} list-group-item list-group-item-action bg-light"><i class="fa fa-bar-chart"></i> &nbsp;Statistik</a>
+<a href="{{ url('/user/summary') }}" class="{{ request()->is('user/summary') ? 'active-btn' : '' }} list-group-item list-group-item-action bg-light"><i class="fa fa-print"></i> &nbsp;Summary</a>
 @endsection
 <!-- end menu sidebar -->
 
 @section('menu-collapse')
 <li class="nav-item collap">
-  <a class="{{ request()->is('user/home') ? 'active-btn' : '' }} nav-link" href="{{ url('/user/home') }}"><i class="fa fa-home"></i> Home</a>
+  <a class="{{ request()->is('user/home') ? 'active-btn' : '' }} nav-link" href="{{ url('/user/home') }}"><i class="fa fa-home"></i>&nbsp; Home</a>
 </li>
 <li class="nav-item collap">
-  <a class="{{ request()->is('user/lapor') ? 'active-btn' : '' }} nav-link" href="{{ url('/user/lapor') }}"><i class="fa fa-volume-up"></i> Lapor Bahaya</a>
+  <a class="{{ request()->is('user/lapor') ? 'active-btn' : '' }} nav-link" href="{{ url('/user/lapor') }}"><i class="fa fa-volume-up"></i>&nbsp; Lapor Bahaya</a>
 </li>
 <li class="nav-item collap">
-  <a class="{{ request()->is('user/riwayat') ? 'active-btn' : '' }} nav-link" href="{{ url('/user/riwayat') }}"><i class="fa fa-history"></i> Riwayat pelaporan</a>
+  <a class="{{ request()->is('user/riwayat') ? 'active-btn' : '' }} nav-link" href="{{ url('/user/riwayat') }}"><i class="fa fa-history"></i>&nbsp; Riwayat pelaporan</a>
 </li>
 <li class="nav-item collap">
-  <a class="{{ request()->is('user/statistik') ? 'active-btn' : '' }} nav-link" href="{{ url('/user/statistik') }}"><i class="fa fa-bar-chart"></i> Statistik</a>
+  <a class="{{ request()->is('user/statistik') ? 'active-btn' : '' }} nav-link" href="{{ url('/user/statistik') }}"><i class="fa fa-bar-chart"></i>&nbsp; Statistik</a>
 </li>
 <li class="nav-item collap">
-  <a class="{{ request()->is('user/summary') ? 'active-btn' : '' }} nav-link" href="{{ url('/user/summary') }}"><i class="fa fa-print"></i> Summary</a>
+  <a class="{{ request()->is('user/summary') ? 'active-btn' : '' }} nav-link" href="{{ url('/user/summary') }}"><i class="fa fa-print"></i>&nbsp; Summary</a>
 </li>
 @endsection
--- resources/views/pages/verifikator/lapor_bahaya.blade.php
+++ resources/views/pages/verifikator/lapor_bahaya.blade.php
@@ -5,68 +5,47 @@
 @section('content')
 <!-- content home -->
   <div class="container" >
-    <form class="" action="index.html" method="post">
-    <div class="row">
+    <form class="" action="{{ url('/verifikator/lapor') }}" method="post">@csrf
+
+    <br><h5>Form Pelaporan Bahaya</h5>
+
+    @if ($errors->any())
+    <br><small>
+      <div class="alert alert-danger">
+          @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+          @endforeach
+      </div>
+    </small>
+    @endif
+
+    @if (session('success'))
+      <br><small>
+        <div class="alert alert-success"> {{ session('success') }} </div>
+      </small>
+    @endif
+
+    <div class="row" style="margin-top: -20px">
       <div class="col-lg-6">
         <div class="report-form-container">
           <div class="form-group">
-            <label for="exampleFormControlSelect1">Section</label>
-            <select class="form-control" onchange="showInputSection(this)">
-              <option>Business</option>
-              <option>Unit</option>
-              <option>Produksi</option>
-              <option>Engineering</option>
-              <option>Plant</option>
-              <option>MCD</option>
-              <option>PSCM</option>
-              <option>LC&D</option>
-              <option>SHE</option>
-              <option>BE</option>
-              <option>GS</option>
-              <option>HR</option>
-              <option>IER</option>
-              <option>Finance</option>
-              <option>IT</option>
-              <option value="other">Other</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <input type="input" class="form-control" id="other-section-input" placeholder="Ketik disini" style="display:none; margin-top:20px;">
-          </div>
-          <div class="form-group">
-            <label for="exampleFormControlSelect1">BRL/Level</label>
-            <select class="form-control"  onchange="showInputLevel(this)">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-              <option>6</option>
-              <option>Vendor/Mitra Kerja</option>
-              <option value="other">Other</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <input type="input" class="form-control" id="other-level-input" placeholder="Ketik disini" style="display:none; margin-top:20px;">
-          </div>
-          <div class="form-group">
             <label for="exampleFormControlSelect2">Tanggal ditemukannya bahaya (bulan/hari/tahun)</label>
+            <!-- <input id="datepicker" width="276" /> -->
             <div class="form-group">
                 <div class="input-group date" id="datetimepicker" data-target-input="nearest">
-                    <input type="text" class="form-control datetimepicker-input" data-target="#datetimepicker"/>
+                    <input type="text" name="date" required value="{{ old('date') }}" class="form-control datetimepicker-input" data-target="#datetimepicker" placeholder="Klik icon disamping ->"/>
                     <div class="input-group-append" data-target="#datetimepicker" data-toggle="datetimepicker">
                         <div class="input-group-text"><i class="fa fa-calendar"></i></div>
                     </div>
                 </div>
             </div>
 
-
           </div>
           <div class="form-group">
             <label for="exampleFormControlSelect2">Waktu ditemukannya bahaya</label>
             <div class="form-group">
                 <div class="input-group date" id="datetimepicker2" data-target-input="nearest">
-                    <input type="text" class="form-control datetimepicker-input" data-target="#datetimepicker2"/>
+                    <input type="text" name="time" required value="{{ old('time') }}" class="form-control datetimepicker-input" data-target="#datetimepicker2" placeholder="Klik icon disamping ->"/>
                     <div class="input-group-append" data-target="#datetimepicker2" data-toggle="datetimepicker">
                         <div class="input-group-text"><i class="fa fa-clock-o"></i></div>
                     </div>
@@ -75,39 +54,35 @@
           </div>
           <div class="form-group">
             <label for="exampleFormControlSelect2">Lokasi</label>
-            <select class="form-control" id="exampleFormControlSelect2">
-              <option>Office</option>
-              <option>Warehouse</option>
-              <option>Workshop</option>
-              <option>Area Tambang (OB)</option>
-              <option>Area Tambang (Coal)</option>
-              <option>Area Mess</option>
-              <option>Pit Stop/ Shutdown</option>
-              <option>Area Lainnya</option>
+            <select class="form-control" name="location" id="exampleFormControlSelect2">
+              <option value="Office">Office</option>
+              <option value="Warehouse">Warehouse</option>
+              <option value="Workshop">Workshop</option>
+              <option value="Area Tambang">Area Tambang (OB)</option>
+              <option value="Area Tambang (Coal)">Area Tambang (Coal)</option>
+              <option value="Area Mess">Area Mess</option>
+              <option value="Pit Stop/Shutdown">Pit Stop / Shutdown</option>
+              <option value="Area Lainnya">Area Lainnya</option>
             </select>
           </div>
           <div class="form-group">
             <label for="exampleFormControlTextarea1">Detail Lokasi</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="2" placeholder="Contoh: Pit M sisi timur front PC2000-30"></textarea>
+            <textarea class="form-control" name="detail_location" id="exampleFormControlTextarea1" rows="2" placeholder="Contoh: Pit M sisi timur front PC2000-30">{{ old('detail_location') }}</textarea>
           </div>
-        </div>
-      </div>
-      <div class="col-lg-6">
-        <div class="report-form-container">
           <div class="grid-container">
             <div class="grid-area-1">
               <div class="form-group">
                   <label for="exampleFormControlSelect1">Kategori Bahaya</label>
-                  <select class="form-control" id="exampleFormControlSelect1">
-                    <option> Kondisi Tidak Aman </option>
-                    <option> Tindakan Tidak Aman</option>
+                  <select class="form-control" name="danger_category" id="exampleFormControlSelect1">
+                    <option value="Kondisi Tidak Aman"> Kondisi Tidak Aman </option>
+                    <option value="Tindakan Tidak Aman"> Tindakan Tidak Aman</option>
                   </select>
                 </div>
               </div>
               <div class="grid-area-2">
                 <div class="form-group">
                     <label for="exampleFormControlSelect1">Kode Bahaya</label>
-                    <select class="form-control" id="exampleFormControlSelect1">
+                    <select class="form-control" name="danger_code" id="exampleFormControlSelect1">
                       <option>AA</option>
                       <option>A</option>
                       <option>B </option>
@@ -116,23 +91,27 @@
                   </div>
               </div>
           </div>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="report-form-container">
           <div class="form-group">
             <label for="exampleFormControlSelect1">Deskripsi Bahaya</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="2" placeholder="Contoh: Ditemukan tinggi tanggul yang kurang dari 3/4 tinggi tyre HD"></textarea>
+            <textarea class="form-control" required name="description" id="exampleFormControlTextarea1" rows="2" placeholder="Contoh: Ditemukan tinggi tanggul yang kurang dari 3/4 tinggi tyre HD">{{ old('description') }}</textarea>
           </div>
           <div class="form-group">
             <label for="exampleFormControlSelect2">Resiko</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="2" placeholder="Contoh: Unit terjatuh karena tidak ada pengamanan"></textarea>
+            <textarea class="form-control" required name="risk" id="exampleFormControlTextarea1" rows="2" placeholder="Contoh: Unit terjatuh karena tidak ada pengamanan">{{ old('risk') }}</textarea>
           </div>
           <div class="form-group">
             <label for="exampleFormControlSelect2">Tindakan Perbaikan</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="2" placeholder="Contoh: Melakukan peninggian tanggul 3/4 dari tinggi tyre HD sesuai dengan standar"></textarea>
+            <textarea class="form-control" required name="action" id="exampleFormControlTextarea1" rows="2" placeholder="Contoh: Melakukan peninggian tanggul 3/4 dari tinggi tyre HD sesuai dengan standar">{{ old('action') }}</textarea>
           </div>
           <div class="form-group">
             <label for="exampleFormControlSelect2">Status</label>
-            <select class="form-control" id="exampleFormControlSelect2">
-              <option>Open</option>
-              <option>Close</option>
+            <select class="form-control" name="status" id="exampleFormControlSelect2">
+              <option value="Open">Open</option>
+              <option value="Close">Close</option>
             </select>
           </div>
           <button type="submit" class="btn btn-primary form-control">Submit Laporan</button>
